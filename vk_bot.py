8df7f6a78b3041fce2f2981e561f490585c351a4
vanja97 <gavr97.97@mail.ru>
2020-02-10 23:06:31 +0300

    Создание бота

---
+++ vk_bot.py
@@ -0,0 +1,37 @@
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+# получает id пользователя ВК, которому отправляет сообщение и получает само сообщение
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message})
+
+
+# API-key
+token = '80986717bf112020b71cfbdb4cc8bdb2d473ff0550e83a86d4bfd2a75b69e4842cd9109462679a3f467b1'
+
+# Авторизуемся как сообщество
+vk = vk_api.VkApi(token = token)
+
+# Работа с сообщениями
+longopoll = VkLongPoll(vk)
+
+# Основной цикл
+for even in longopoll.listen():
+
+    # Если пришло новое сообщение
+    if even.type == VkEventType.MESSAGE_NEW:
+
+        # Если онго имеет метку для бота
+        if even.to_me:
+
+            # Сообщение от пользователя
+            request = even.text
+
+            # Логика ответа
+            if request == 'привет':
+                write_msg(even.user_id, 'Привет)..')
+            elif request == 'Пока':
+                write_msg(even.user_id, 'Пока((')
+            else:
+                write_msg(even.user_id, 'Не понял логики вашего выражения...')
+
